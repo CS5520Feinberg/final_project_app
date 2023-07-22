17d696e0aa2d681a3ead143c4bad0951a3de4177
Jialin H <lucyhuangpsy@gmail.com>
2023-07-22 13:02:01 -0400

    Updated WeeklyChartActivity into combinedChart - need to change export to FALSE in the production

--- app/src/main/java/edu/northeastern/final_project/WeeklyChartActivity.java
+++ app/src/main/java/edu/northeastern/final_project/WeeklyChartActivity.java
@@ -3,118 +3,143 @@ package edu.northeastern.final_project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.model.GradientColor;
+import com.google.firebase.database.collection.LLRBNode;
 
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class WeeklyChartActivity extends AppCompatActivity {
 
-    private BarChart chart;
-    private SeekBar seekBarX, seekBarY;
-    private TextView tvX, tvY;
+    private CombinedChart chart;
+    final Integer itemcount = 12;
 
+    private Random rand = new Random();
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_chart);
 
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-    setContentView(R.layout.activity_weekly_chart);
+        chart = findViewById(R.id.weeklyCombinedChart);
 
-    setTitle("WeeklyChartActivity");
+        setupChart();
 
-    chart.setDrawBarShadow(false);
-    chart.setDrawValueAboveBar(true);
-    chart.getDescription().setEnabled(false);
-    chart.setMaxVisibleValueCount(60);
-    chart.setPinchZoom(false);
-    chart.setDrawGridBackground(false);
+    }
 
-    Legend l = chart.getLegend();
+    /*** reference: https://stackoverflow.com/questions/33240002/mpandroidchart-combined-chart-bar-line-with-space***/
+    private void setupChart() {
 
-    l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-    l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
-    l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-    l.setDrawInside(false);
-    l.setForm(Legend.LegendForm.SQUARE);
-    l.setFormSize(9f);
-    l.setTextSize(11f);
-    l.setXEntrySpace(4f);
+        chart.setDrawOrder(new CombinedChart.DrawOrder[] { CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE});
 
-    setData(5, 100);
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getXAxis().setDrawGridLines(false);
+
+        XAxis x = chart.getXAxis();
+        x.setTextSize(14f);
+        x.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        YAxis y = chart.getAxisLeft();
+        y.setEnabled(false);
+
+        chart.setDrawGridBackground(false);
+        chart.setDrawBarShadow(false);
+        chart.setVisibleXRangeMaximum(10f);
+        chart.setVisibleXRangeMinimum(10f);
+        chart.getDescription().setEnabled(false);
+
+        CombinedData data = new CombinedData();
+
+        data.setData(getLineData());
+        data.setData(getBarData());
+        chart.setData(data);
+        chart.invalidate();
+
+        Legend legend = chart.getLegend();
+        legend.setTextSize(14f);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
     }
 
 
-    private void setData (int count, float range) {
-        float start = 1f;
-        ArrayList<BarEntry> values = new ArrayList<>();
+    private LineData getLineData() {
 
-        /*** TODO: mockuop data - need to be replaced with firebase generated data ***/
-        for (int i = (int) start; i<start+count; i++) {
-            float val = (float) (Math.random()*(range+1));
+        /*** TODO - change the data by pulling from firebase***/
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+        //ArrayList<String> labels = new ArrayList<String>();
 
-            if (Math.random()*100<25) {
-                values.add(new BarEntry(i, val, getResources().getDrawable(R.drawable.star)));
-            } else {
-                values.add(new BarEntry(i, val));
-            }
+        LineData ld = new LineData();
 
-            BarDataSet set1;
-
-            if ((chart.getData() !=null) && (chart.getData().getDataSetCount() > 0)) {
-                set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
-                set1.setValues(values);
-                chart.getData().notifyDataChanged();
-                chart.notifyDataSetChanged();
-            } else {
-                set1 = new BarDataSet(values, "The year 2017");
-                set1.setDrawIcons(false);
-
-                int startColor1 = ContextCompat.getColor(this, android.R.color.holo_orange_light);
-                int startColor2 = ContextCompat.getColor(this, android.R.color.holo_blue_light);
-                int startColor3 = ContextCompat.getColor(this, android.R.color.holo_orange_light);
-                int startColor4 = ContextCompat.getColor(this, android.R.color.holo_green_light);
-                int startColor5 = ContextCompat.getColor(this, android.R.color.holo_red_light);
-                int endColor1 = ContextCompat.getColor(this, android.R.color.holo_blue_dark);
-                int endColor2 = ContextCompat.getColor(this, android.R.color.holo_purple);
-                int endColor3 = ContextCompat.getColor(this, android.R.color.holo_green_dark);
-                int endColor4 = ContextCompat.getColor(this, android.R.color.holo_red_dark);
-                int endColor5 = ContextCompat.getColor(this, android.R.color.holo_orange_dark);
-
-                List<GradientColor> gradientFills = new ArrayList<>();
-
-                gradientFills.add(new GradientColor(startColor1, endColor1));
-                gradientFills.add(new GradientColor(startColor2, endColor2));
-                gradientFills.add(new GradientColor(startColor3, endColor3));
-                gradientFills.add(new GradientColor(startColor4, endColor4));
-                gradientFills.add(new GradientColor(startColor5, endColor5));
-
-                set1.setGradientColors(gradientFills);
-
-                ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-                dataSets.add(set1);
-
-                BarData data = new BarData(dataSets);
-                data.setValueTextSize(10f);
-                data.setBarWidth(0.9f);
-
-                chart.setData(data);
-
-            }
+        for (int index = 0; index< itemcount; index++) {
+            entries.add(new Entry(index+0.5f, rand.nextInt(15)));
         }
+
+        LineDataSet lset = new LineDataSet(entries, "Target Dataset");
+        lset.setColor(Color.RED);
+        lset.setLineWidth(2.5f);
+        lset.setCircleColor(Color.RED);
+        lset.setCircleSize(5f);
+        lset.setFillColor(Color.WHITE);
+        //lset.setDrawCubic(true);
+        lset.setDrawValues(true);
+        lset.setValueTextSize(10f);
+        lset.setValueTextColor(Color.RED);
+        lset.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        ld.addDataSet(lset);
+
+        Log.d("TAG", "lineData was created");
+
+        return ld;
+    }
+
+    private BarData getBarData() {
+        //ArrayList<String> labels = new ArrayList<String>();
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < itemcount; i++) {
+            entries.add(new BarEntry(i, rand.nextInt(15)));
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, "BarDataSet");
+
+        barDataSet.setColor(Color.parseColor("#285ded"));
+
+        BarData bd = new BarData(barDataSet);
+        bd.addDataSet(barDataSet);
+
+        Log.d("TAG", "BarData was created");
+
+        return bd;
     }
 }
