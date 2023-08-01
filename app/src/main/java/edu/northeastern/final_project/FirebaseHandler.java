bb2cd8d0eae9513bf6944836e23d0b8def296280
Jialin H <lucyhuangpsy@gmail.com>
2023-08-01 12:31:56 -0400

    Restructure the FirebaseHandler and need to fix different bugs.

--- app/src/main/java/edu/northeastern/final_project/FirebaseHandler.java
+++ app/src/main/java/edu/northeastern/final_project/FirebaseHandler.java
@@ -3,6 +3,8 @@ package edu.northeastern.final_project;
 import static androidx.core.content.ContextCompat.getSystemService;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.util.Log;
@@ -17,18 +19,26 @@ public class FirebaseHandler {
     private DBHandler dbHandler;
     private ArrayList<Intake> intakeData;
 
-    //check whether the device is online
+    // read data from sqlite
+    intakeData = readIntake();
+    //update sqlite if data was pushed
+    if (isOnLine() && intakeData.size()>0) {
+            pushFirebase(intakeData);
+    }
+
+    // check whether the device is online
     public boolean isOnLine() {
 
         ConnectivityManager connectivityManager = (ConnectivityManager) Context.getSystemService(DailyIntakeActivity.this);
 
-        connectivityManager.registerDefaultNetworkCallback(new ConnectivityManager.NetworkCallback(){
+        connectivityManager.registerDefaultNetworkCallback(new ConnectivityManager.NetworkCallback() {
                                                                @Override
                                                                public void onAvailable(Network network) {
                                                                    Variables.isNetworkConnected = true; // Global Static Variable
                                                                    Log.d("Network Connection", "success");
                                                                }
-                                                               @Override
+
+            @Override
                                                                public void onLost(Network network) {
                                                                    Variables.isNetworkConnected = false; // Global Static Variable
                                                                    Log.d("Network Connection", "fail");
@@ -42,17 +52,31 @@ public class FirebaseHandler {
         } else {
             return false;
         }
+    }
 
-        //if the sqlite updates, push the data into firebase after the click button on add data intake
-        // read data from sqlite
-        dbHandler = new DBHandler(DailyIntakeActivity.this);
-        intakeData = dbHandler.readIntake();
 
-        //update sqlite if data was pushed
-        pushFirebase(intakeData);
+    //read data from database
+    public ArrayList<Intake> readIntake() {
+        SQLiteDatabase db = this.getReadableDatabase();
 
-        //
+        Cursor cursorIntake = db.rawQuery("SELECT * FROM appdb", null);
 
+        ArrayList<Intake> intakeArrayList = new ArrayList<>();
+
+        if (cursorIntake.moveToFirst()) {
+            do {
+                intakeArrayList.add (new Intake(cursorIntake.getString(1),
+                        cursorIntake.getString(2),
+                        cursorIntake.getString(3),
+                        cursorIntake.getString(4),
+                        cursorIntake.getString(5),
+                        cursorIntake.getString(6))) ;
+
+            } while (cursorIntake.moveToNext());
+        }
+
+        cursorIntake.close();
+        return intakeArrayList;
     }
 
     //push intake into firebase - sync the data
