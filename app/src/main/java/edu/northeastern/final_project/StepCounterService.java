ebb38b889b517f9a5d3d6a1993dd3146a45e23b4
Shashank Manjunath <shashankmanjunath14@gmail.com>
2023-08-11 14:54:30 -0400

    Updated DBHandler to eliminate duplicate uploads

--- app/src/main/java/edu/northeastern/final_project/StepCounterService.java
+++ app/src/main/java/edu/northeastern/final_project/StepCounterService.java
@@ -1,8 +1,5 @@
 package edu.northeastern.final_project;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -15,7 +12,6 @@ import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.firebase.auth.FirebaseAuth;
